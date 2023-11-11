22d4bc5513300a891ce3324bf5889b8a89e73cef
Thomas Mosmant <thomas.mosmant@gmail.com>
2023-11-11 16:13:43 +0100

    simplify things

--- App.js
+++ App.js
@@ -1,54 +1,13 @@
 import * as React from "react";
-import * as WebBrowser from "expo-web-browser";
-import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
-import { Button, View, StyleSheet } from "react-native";
 
-WebBrowser.maybeCompleteAuthSession();
+import Router from "./router";
 
-const discovery = {
-  authorizationEndpoint: "https://www.strava.com/oauth/mobile/authorize",
-  tokenEndpoint: "https://www.strava.com/oauth/token",
-  revocationEndpoint: "https://www.strava.com/oauth/deauthorize",
-};
+import { AuthProvider } from "./store/AuthContext";
 
 export default function App() {
-  const [request, response, promptAsync] = useAuthRequest(
-    {
-      clientId: process.env.EXPO_PUBLIC_STRAVA_CLIENT_ID,
-      scopes: ["activity:read_all"],
-      redirectUri: makeRedirectUri({
-        native: process.env.EXPO_PUBLIC_STRAVA_REDIRECT_URL,
-        preferLocalhost: true,
-      }),
-    },
-    discovery
-  );
-
-  React.useEffect(() => {
-    if (response?.type === "success") {
-      const { code } = response.params;
-
-      console.log(code);
-    }
-  }, [response]);
-
   return (
-    <View style={styles.container}>
-      <Button
-        disabled={!request}
-        title="Login"
-        onPress={() => {
-          promptAsync();
-        }}
-      />
-    </View>
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-  },
-});
